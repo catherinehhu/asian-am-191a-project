911eaf26afa5929ad74bc3024516bec18f545361
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2023-06-01 09:44:35 -0700

    base map fixes

--- js/script.js
+++ js/script.js
@@ -42,12 +42,12 @@ const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
 L.control.layers(null,layers).addTo(map)
 
 //map
-var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxZoom: 16, 
 });
 
-Stadia_AlidadeSmooth.addTo(map);
+Esri_WorldGrayCanvas.addTo(map);
 
 function addMarker(data){
     if(data['Please provide the zip code of your primary residence. / Por favor ingrese el còdigo postal de su residencia principal.'] == "90232"){
