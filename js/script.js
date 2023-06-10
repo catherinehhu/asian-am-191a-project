024a8a32a7562c944c2bddb4dd2a3f72e27cf4b8
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2023-06-10 16:01:33 -0700

    hadjsfgjdhg

--- js/script.js
+++ js/script.js
@@ -103,10 +103,7 @@ function processData(results){
     results.data.forEach(data => {
         console.log(data)
         addMarker(data)
-        // if(data["Please describe why you think this is the case."]){
-        //     createList(data.lat, data.lng, data)
-        // }
-
+        createStory(data.lat, data.lng, data)
     })
     zip1.addTo(map) // add our layers after markers have been made
     zip2.addTo(map) // add our layers after markers have been made  
@@ -122,26 +119,22 @@ function processData(results){
     map.fitBounds(allLayers.getBounds());
 }
 
-// function createList(lat,lng,data){
-//     const item = document.createElement("list"); 
-//     item.id = "button"; 
-//     const itemspace = document.getElementById("stories");
-//     itemspace.appendChild(item); 
-//     // Button Text 
-//     let text1 = data["Please describe why you think this is the case."];
-//     // let text2 = "Is there anything else you'd like to share? " + data["Is there anything else you would like to share about your experiences with oil drilling in Los Angeles?"];  
-//         item.innerHTML = `<p>${text1}</p>`; 
-//         // item.innerHTML += "\n"; 
-//         // item.innerHTML += `<li>${text2}</li>`; 
-//         // console.log(text)
-//     // Button Styling
-//     item.style.margin = "10px";
-//     item.style.color = "black"; 
-// }
+
+function createStory(lat,lng,data){
+    const item = document.createElement("list"); 
+    item.id = "button"; 
+    const itemspace = document.getElementById("stories");
+    itemspace.appendChild(item); 
+    let text1 = data["Please describe why you think this is the case."];
+    let text2 = "Is there anything else you'd like to share? " + data["Is there anything else you would like to share about your experiences with oil drilling in Los Angeles?"];  
+        item.innerHTML = `<p>${text1}</p>`; 
+        item.innerHTML += "\n"; 
+        item.innerHTML += `<li>${text2}</li>`; 
+}
 
 loadData(dataUrl)
 
-function openCity(evt, cityName) {
+function openTab(evt, tabname) {
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -151,7 +144,7 @@ function openCity(evt, cityName) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace("active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tabname).style.display = "block";
     evt.currentTarget.className += " active";
   }
   
