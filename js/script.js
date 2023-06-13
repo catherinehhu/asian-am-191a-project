d3c937a2a29d155a921213f1f985c866b1909ef0
Catherine Hu <64565524+catherinehhu@users.noreply.github.com>
2023-06-12 20:49:14 -0700

    styling

--- js/script.js
+++ js/script.js
@@ -49,7 +49,7 @@ function loadData(url){
 }
 
 loadData(dataUrl)
-document.getElementById("summary").innerHTML = "Hover your mouse over the map to activate shading.<br><br>Click on any zip code to view community stories under the Stories tab. A graphical summary of responses will be displayed here in the Summary tab."; 
+document.getElementById("stories").innerHTML = "Hover your mouse over the map to activate shading.<br><br>Click on any zip code to view community stories under the Stories tab. A graphical summary of responses will be displayed here in the Summary tab."; 
 
 
 let shading = {
